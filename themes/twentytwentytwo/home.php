062da43ba3480d670a68fb99d6d4e7af347807ab
Aquarius <frederic.nobre@gmail.com>
2022-10-30 01:35:39 +0200

    ça marche, plus qu'à voir comment custom le theme d'un post

---
+++ themes/twentytwentytwo/home.php
@@ -0,0 +1,58 @@
+<?php
+    /*
+     * Template Name: Homepage__pikachu
+     */
+?>
+<!-- This page is generated outside of WP by: /wp-content/themes/CURRENT-THEME/home.php -->
+<!-- Your HTML Code Here -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="work in progress" />
+    <meta name="author" content="aquarius" />
+    <title>Raphael Levy's, under construction ...</title>
+    <link rel="shortcut icon" href="./images/icons/alogo-1.svg">
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&amp;display=swap" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap" rel="stylesheet" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+    <!-- Background Video-->
+    <video aria-describedby="cards" class="bg-video" playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop"><source src="assets/mp4/shuffle-cards.mp4" type="video/mp4" /></video>
+    <!-- Masthead-->
+    <div class="masthead">
+        <div class="masthead-content text-white">
+            <div class="container-fluid px-4 px-lg-0">
+                <h1 class="fst-italic lh-1 mb-4">YAY Our website will soon be online</h1>
+                <p class="mb-5">We are working hard to complete the development of the site. In the meantime, feel free to follow me on my social networks ;)</p>
+            </div>
+        </div>
+    </div>
+    <!-- Social Icons-->
+    <!-- For more icon options, visit https://fontawesome.com/icons?d=gallery&p=2&s=brands-->
+    <div class="social-icons">
+        <div class="d-flex flex-row flex-lg-column justify-content-center align-items-center h-100 mt-3 mt-lg-0">
+            <a class="btn btn-dark m-3" href="https://twitter.com/raphlevymtg"><i class="fab fa-twitter"></i></a>
+            <a class="btn btn-dark m-3" href="https://www.facebook.com/"><i class="fab fa-facebook"></i></a>
+            <a class="btn btn-dark m-3" href="https://www.youtube.com/channel/UCelnNVqwjQpi7ttIREZ2A6A"><i class="fab fa-youtube"></i></a>
+            <a class="btn btn-dark m-3" href="https://www.twitch.tv/raphaellevy"><i class="fab fa-twitch"></i></a>
+            <a class="btn btn-dark m-3" href="https://discord.com/channels/627111574200385536/627111574200385538"><i class="fab fa-discord"></i></a>
+        </div>
+    </div>
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+
+</html>
